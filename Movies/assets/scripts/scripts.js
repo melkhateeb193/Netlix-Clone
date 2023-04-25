c899c45d3da6a95063ff8f1437006bd37c53e4bb
abdo <abdoalaaabdo17@gmail.com>
2023-04-25 12:22:36 +0200

    Movies 0.0.1

---
+++ Movies/assets/scripts/scripts.js
@@ -0,0 +1,25 @@
+//search bar
+let search = document.querySelector(".search");
+let searchIcon = document.querySelector(".search__icon");
+let searchInput = document.querySelector(".search__input");
+let searchClose = document.querySelector(".search__close");
+
+searchIcon.addEventListener("click", () => {
+    search.classList.add("search__open");
+    searchInput.focus();
+})
+
+searchClose.addEventListener("click", () => {
+    search.classList.remove("search__open");
+    searchInput.value = "";
+})
+///////////////////////////////////////////////////////////////////////////////////
+
+//genres list
+let grid = document.getElementsByClassName("genres__grid")[0];
+function opengenres() {
+    if (grid.style.display === 'none') {
+        grid.style.display = 'grid';
+    }
+    else { grid.style.display = 'none' }
+}
